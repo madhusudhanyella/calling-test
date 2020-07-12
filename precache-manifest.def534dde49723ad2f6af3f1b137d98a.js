13b7460804d93d1a2c3035b161777598c929d591
MADHU SUDHAN <madhusudhan.yella99@gmail.com>
2020-07-12 22:12:13 +0530

    Add files via upload

---
+++ precache-manifest.def534dde49723ad2f6af3f1b137d98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe32d22b293f4e97ed62d2b7e92daac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8542e5ad981a47dffde3",
+    "url": "/static/css/main.bc776159.chunk.css"
+  },
+  {
+    "revision": "9293068e8af969e55dab",
+    "url": "/static/js/2.901212be.chunk.js"
+  },
+  {
+    "revision": "1a73d23dba27d2dba2e09dcf94770e73",
+    "url": "/static/js/2.901212be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8542e5ad981a47dffde3",
+    "url": "/static/js/main.61d1efd0.chunk.js"
+  },
+  {
+    "revision": "bd3829c64a41a153a3ed",
+    "url": "/static/js/runtime-main.8a59c522.js"
+  }
+]);
